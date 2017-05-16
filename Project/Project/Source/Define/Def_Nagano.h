71d70fafb3218f30572ce19e774bd6a9bb87f1b1
G016A1550\nakayama <alex.fuki@gmail.com>
2017-05-16 13:50:11 +0900

    スクリーンの大きさの変更と背景画像の大きさの変更

--- Project/Project/Source/Define/Def_Nagano.h
+++ Project/Project/Source/Define/Def_Nagano.h
@@ -2,7 +2,7 @@
 
 #include <GSvector2.h>
 
-static const GSvector2 SCREEN_SIZE = GSvector2(1024, 768);
+static const GSvector2 SCREEN_SIZE = GSvector2(1280, 960);
 
 static const float MINUS = -1.0f;
 static const float PLUS = 1.0f;
